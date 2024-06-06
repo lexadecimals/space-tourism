f10daa2210a247a55fb5fef682c5e3a30fa41c28
lexadecimals <alexis.wilkinsondefoe@gmail.com>
2024-06-06 11:23:52 +0100

    restyles destination page

--- src/components/location/index.jsx
+++ src/components/location/index.jsx
@@ -1,21 +1,34 @@
 import data from '../../data.json';
+import styles from './style.module.css';
 
-const Location = ({ name }) => {
+const Location = ({ name, children }) => {
   const body = data.destinations.filter(
     (e) => e.name.toLowerCase() === name
   )[0];
   return (
-    <div>
+    <>
       {body && (
         <>
-          <h1>{name}</h1>
-          <img src={`../../${body.images.png}`} alt="" />
-          <p>{body.description}</p>
-          <p>avg distance: {body.distance}</p>
-          <p>est travel time: {body.distance}</p>
+          <img src={`../../${body.images.png}`} alt="" className={styles.img} />
+          <div className={styles['content-location']}>
+            {children}
+            <h2 className={styles['heading-location-l']}>{name}</h2>
+            <p className={styles['description-location']}>{body.description}</p>
+            <hr />
+            <div className={styles.facts}>
+              <p className={styles['fact-location']}>
+                avg. distance:<br></br>
+                <span>{body.distance}</span>
+              </p>
+              <p className={styles['fact-location']}>
+                est. travel time: <br></br>
+                <span>{body.travel}</span>
+              </p>
+            </div>
+          </div>
         </>
       )}
-    </div>
+    </>
   );
 };
 
--- src/pages/destination/index.jsx
+++ src/pages/destination/index.jsx
@@ -2,31 +2,34 @@ import { NavLink, useParams } from 'react-router-dom';
 
 import Location from '../../components/location/index.jsx';
 import data from '../../data.json';
+import styles from './style.module.css';
 
 const Destination = () => {
   const { name } = useParams();
   const destinations = data.destinations;
 
   return (
-    <>
-      <h1>Pick Your Destination</h1>
-      <nav>
-        <ul>
-          {destinations &&
-            destinations.map((location) => {
-              return (
-                <li key={location.description}>
-                  <NavLink to={`/destination/${location.name.toLowerCase()}`}>
-                    {location.name}
-                  </NavLink>
-                </li>
-              );
-            })}
-        </ul>
-      </nav>
-      <Location name={name} />
-      {/* <p>{data[name]}</p> */}
-    </>
+    <div className={styles['container-destination']}>
+      <h2 className={styles['heading-destination-xs']}>
+        <span>01</span> Pick Your Destination
+      </h2>
+      <Location name={name}>
+        <nav>
+          <ul>
+            {destinations &&
+              destinations.map((location) => {
+                return (
+                  <li key={location.description}>
+                    <NavLink to={`/destination/${location.name.toLowerCase()}`}>
+                      {location.name}
+                    </NavLink>
+                  </li>
+                );
+              })}
+          </ul>
+        </nav>
+      </Location>
+    </div>
   );
 };
 
